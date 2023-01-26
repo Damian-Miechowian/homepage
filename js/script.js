d548e0114f4ac465d1437e1a0e196c630c488eea
Damian-Miechowian <121828192+Damian-Miechowian@users.noreply.github.com>
2023-01-26 15:27:33 +0100

    Third commit - used of functions in the code

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
-console.log("Witam sedecznie wszystkich developerów!");
-
-let button = document.querySelector(".js-button");
-let secondButton = document.querySelector(".js-secondButton");
-
-let body = document.querySelector(".body");
-let nameTheme = document.querySelector(".js-button__nameTheme");
-
-let header = document.querySelector(".js-header");
-// let secondHeadline = document.querySelector(".js-secondHeadline");
-// // console.log(".button");
-// console.log(".button__secondButton");
+{
+    const welcome = () => {
+        console.log("Witam sedecznie wszystkich developerów!");
+    }
 
 
-button.addEventListener("click", () => {
-    header.classList.toggle("headline__secondHeadline");
-});
-
-secondButton.addEventListener("click", () => {
-    body.classList.toggle("body--grey");
-    nameTheme.innerText = body.classList.contains("body--grey") ? "jasny" : "ciemny";
-});
+    const toggleHeadlineColor = () => {
+        const header = document.querySelector(".js-header");
+        header.classList.toggle("headline__secondHeadline");
+    };
 
 
+    const toggleBackground = () => {
+        const body = document.documentElement;
+        const nameTheme = document.querySelector(".js-button__nameTheme");
+
+        body.classList.toggle("body--grey");
+        nameTheme.innerText = body.classList.contains("body--grey") ? "jasny" : "ciemny";
+    };
+
+
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        const secondButton = document.querySelector(".js-secondButton");
+
+        button.addEventListener("click", toggleHeadlineColor);
+        secondButton.addEventListener("click", toggleBackground);
+
+        welcome();
+    };
+
+    init();
+}
